101389f87e2de48a33c3abe48fe9b0e1e358f6a1
Miguel Ángel Gallart <magallart58@gmail.com>
2022-10-06 09:13:27 +0200

    tipos array

---
+++ bases/tipos/arrays.ts
@@ -0,0 +1,11 @@
+(() => {
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  numbers.push(11);
+
+  console.log(numbers);
+
+  const villains = ['Omega Rojo', 'Dormammu', 'Duende Verde'];
+
+  villains.forEach((v) => console.log(v.toUpperCase()));
+})();
