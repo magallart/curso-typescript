12664d5911544ec48b1c95cece73c84c97b2de9c
Miguel Ángel Gallart <magallart58@gmail.com>
2022-10-06 09:02:27 +0200

    tipo any

---
+++ bases/tipos/any.js
@@ -0,0 +1,13 @@
+"use strict";
+(() => {
+    let avenger;
+    let exists;
+    let power;
+    avenger = 'Dr Strange';
+    // console.log(avenger.charAt(0));
+    console.log(avenger.charAt(0));
+    avenger = 150.156151;
+    console.log(avenger.toFixed(2));
+    console.log(exists);
+    console.log(power);
+})();
