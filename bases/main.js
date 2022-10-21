e9169f0b4bc03ababcc676084b34ad67301eb3f8
Miguel Ángel Gallart <magallart58@gmail.com>
2022-10-21 13:20:00 +0200

    clases abstractas

--- bases/main.js
+++ bases/main.js
@@ -1,37 +1,28 @@
 "use strict";
 (() => {
-    class Avenger {
+    class Mutante {
         constructor(name, realName) {
             this.name = name;
             this.realName = realName;
-            console.log('Constructor Avenger llamado');
-        }
-        getFullName() {
-            return `${this.name} ${this.realName};`;
         }
     }
-    class Xmen extends Avenger {
-        constructor(name, realName, isMutant) {
-            super(name, realName);
-            this.isMutant = isMutant;
-            console.log('Constructor Xmen llamado');
-        }
-        get fullName() {
-            return `${this.name} - ${this.realName}`;
-        }
-        set fullName(name) {
-            if (name.length < 3) {
-                throw new Error('El nombre debe tener más de 3 letras');
-            }
-            this.name = name;
-        }
-        getFullNameXmen() {
-            console.log(super.getFullName());
+    class Xmen extends Mutante {
+        salvarMundo() {
+            return 'Mundo a salvo!';
         }
     }
-    const wolverine = new Xmen('Wolverine', 'Logan', true);
-    console.log(wolverine.fullName);
-    wolverine.fullName = 'Mi';
-    console.log(wolverine.fullName);
+    class Villain extends Mutante {
+        conquistarMundo() {
+            return 'Mundo conquistado';
+        }
+    }
+    const wolverine = new Xmen('Wolverine', 'Logan');
+    const magneto = new Villain('Magneto', 'Magnus');
+    console.log(wolverine.salvarMundo());
+    console.log(magneto.conquistarMundo());
+    const printName = (character) => {
+        console.log(character.realName);
+    };
+    printName(wolverine);
 })();
 //# sourceMappingURL=main.js.map
