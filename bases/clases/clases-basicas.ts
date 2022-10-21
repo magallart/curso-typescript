bafe9fd768cade8a46f79f50de8df782d91f9865
Miguel Ángel Gallart <magallart58@gmail.com>
2022-10-21 12:24:36 +0200

    métodos de las clases

--- bases/clases/clases-basicas.ts
+++ bases/clases/clases-basicas.ts
@@ -1,14 +1,25 @@
 (() => {
   class Avenger {
     static avgAge: number = 35;
+    static getAvgAge() {
+      return this.name;
+    }
 
     constructor(
       private name: string,
       private team: string,
       public realName?: string
     ) {}
+
+    bio() {
+      return `${this.name} (${this.team})`;
+    }
   }
 
   const antman: Avenger = new Avenger('Antman', 'teamCapitan', 'Scott Lang');
   console.log(antman);
+
+  console.log(antman.bio());
+  console.log(Avenger.getAvgAge());
+  
 })();
--- bases/main.js
+++ bases/main.js
@@ -6,9 +6,17 @@
             this.team = team;
             this.realName = realName;
         }
+        static getAvgAge() {
+            return this.name;
+        }
+        bio() {
+            return `${this.name} (${this.team})`;
+        }
     }
     Avenger.avgAge = 35;
     const antman = new Avenger('Antman', 'teamCapitan', 'Scott Lang');
     console.log(antman);
+    console.log(antman.bio());
+    console.log(Avenger.getAvgAge());
 })();
 //# sourceMappingURL=main.js.map
