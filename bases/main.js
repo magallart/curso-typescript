6b9bfd967ae02b6b0e0812c49b273c735ed3f617
Miguel Ángel Gallart <magallart58@gmail.com>
2022-10-21 13:59:52 +0200

    constructores privados

--- bases/main.js
+++ bases/main.js
@@ -1,28 +1,23 @@
 "use strict";
 (() => {
-    class Mutante {
-        constructor(name, realName) {
+    class Apocalipsis {
+        constructor(name) {
             this.name = name;
-            this.realName = realName;
+        }
+        static callApocalipsis() {
+            if (!Apocalipsis.instance) {
+                Apocalipsis.instance = new Apocalipsis('Soy Apocalipsis el único');
+            }
+            return Apocalipsis.instance;
+        }
+        changeName(newName) {
+            this.name = newName;
         }
     }
-    class Xmen extends Mutante {
-        salvarMundo() {
-            return 'Mundo a salvo!';
-        }
-    }
-    class Villain extends Mutante {
-        conquistarMundo() {
-            return 'Mundo conquistado';
-        }
-    }
-    const wolverine = new Xmen('Wolverine', 'Logan');
-    const magneto = new Villain('Magneto', 'Magnus');
-    console.log(wolverine.salvarMundo());
-    console.log(magneto.conquistarMundo());
-    const printName = (character) => {
-        console.log(character.realName);
-    };
-    printName(wolverine);
+    const apocalipsis1 = Apocalipsis.callApocalipsis();
+    const apocalipsis2 = Apocalipsis.callApocalipsis();
+    const apocalipsis3 = Apocalipsis.callApocalipsis();
+    apocalipsis1.changeName('Xavier');
+    console.log(apocalipsis1, apocalipsis2, apocalipsis3);
 })();
 //# sourceMappingURL=main.js.map
