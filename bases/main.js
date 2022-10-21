8f62453836866946905e6cdc3e898dad524fcfcc
Miguel Ángel Gallart <magallart58@gmail.com>
2022-10-21 12:38:50 +0200

    herencia, extends y protected

--- bases/main.js
+++ bases/main.js
@@ -1,22 +1,26 @@
 "use strict";
 (() => {
     class Avenger {
-        constructor(name, team, realName) {
+        constructor(name, realName) {
             this.name = name;
-            this.team = team;
             this.realName = realName;
+            console.log('Constructor Avenger llamado');
         }
-        static getAvgAge() {
-            return this.name;
-        }
-        bio() {
-            return `${this.name} (${this.team})`;
+        getFullName() {
+            return `${this.name} ${this.realName};`;
         }
     }
-    Avenger.avgAge = 35;
-    const antman = new Avenger('Antman', 'teamCapitan', 'Scott Lang');
-    console.log(antman);
-    console.log(antman.bio());
-    console.log(Avenger.getAvgAge());
+    class Xmen extends Avenger {
+        constructor(name, realName, isMutant) {
+            super(name, realName);
+            this.isMutant = isMutant;
+            console.log('Constructor Xmen llamado');
+        }
+        getFullNameXmen() {
+            console.log(super.getFullName());
+        }
+    }
+    const wolverine = new Xmen('Wolverine', 'Logan', true);
+    console.log(wolverine);
 })();
 //# sourceMappingURL=main.js.map
