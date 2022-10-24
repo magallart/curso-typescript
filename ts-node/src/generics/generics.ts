86f1aa8348ec06378d49715de25b25fde1c72d9c
Miguel Ángel Gallart <magallart58@gmail.com>
2022-10-24 17:06:11 +0200

    funciones genéricas

---
+++ ts-node/src/generics/generics.ts
@@ -0,0 +1,11 @@
+export const printObject = (argument: any) => {
+  console.log(argument);
+};
+
+export function genericFunction<T>(argument: T) {
+  return argument;
+}
+
+export const genericArrowFunction = <T>(argument: T) => {
+  return argument;
+};
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,6 +1,15 @@
-import { Hero } from './classes/Hero';
+import { printObject, genericFunction, genericArrowFunction } from './generics/generics';
 
-const ironman = new Hero('Ironman', 1, 55);
+// printObject(123);
+// printObject(new Date());
+// printObject({ a: 1, b: 2, c: 3 });
+// printObject([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+// printObject('Hola Mundo');
 
-console.log(ironman);
-console.log(ironman.power);
+console.log(genericFunction(3.14159).toFixed(2));
+console.log(genericFunction('Hola Mundo').toUpperCase());
+console.log(genericFunction(new Date()).getDate());
+console.log('--------------');
+console.log(genericArrowFunction(3.14159).toFixed(2));
+console.log(genericArrowFunction('Hola Mundo').toUpperCase());
+console.log(genericArrowFunction(new Date()).getDate());
