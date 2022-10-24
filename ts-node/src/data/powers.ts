57068be7258ce5788dc4f858842c6a6057413106
Miguel Ángel Gallart <magallart58@gmail.com>
2022-10-24 16:10:16 +0200

    exports e imports

---
+++ ts-node/src/data/powers.ts
@@ -0,0 +1,17 @@
+interface Power {
+  id: number;
+  description: string;
+}
+
+const powers: Power[] = [
+  {
+    id: 1,
+    description: 'Money',
+  },
+  {
+    id: 2,
+    description: 'Drugs',
+  },
+];
+
+export default powers;
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,5 +1,12 @@
-import { Hero } from './classes/Hero';
+// import { Hero as SuperHero } from './classes/Hero';
+import * as HeroClases from './classes/Hero';
+import powers from './data/powers';
 
-const ironman = new Hero('Ironman', 1, 55);
+const Hero = 123;
+
+// const ironman = new SuperHero('Ironman', 1, 55);
+const ironman = new HeroClases.Hero('Ironman', 1, 55);
 
 console.log({ ironman });
+
+console.log(powers);
