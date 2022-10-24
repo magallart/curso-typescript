346ea75e003d30bb0a3e62547663301b1151f8d8
Miguel Ángel Gallart <magallart58@gmail.com>
2022-10-24 10:01:40 +0200

    ejercicio interfaces

---
+++ bases/ejercicios/app4.ts
@@ -0,0 +1,83 @@
+// Crear interfaces
+
+// Cree una interfaz para validar el auto (el valor enviado por parametro)
+
+interface Coche {
+  encender: boolean;
+  velocidadMaxima: number;
+  acelerar(): void;
+}
+
+const conducirBatimovil = (auto: Coche): void => {
+  auto.encender = true;
+  auto.velocidadMaxima = 100;
+  auto.acelerar();
+};
+
+const batimovil: Coche = {
+  encender: false,
+  velocidadMaxima: 0,
+  acelerar() {
+    console.log('...... gogogo!!!');
+  },
+};
+
+// Cree una interfaz con que permita utilzar el siguiente objeto
+// utilizando propiedades opcionales
+
+interface Guason {
+  reir?: boolean;
+  comer?: boolean;
+  llorar?: boolean;
+}
+
+const guason: Guason = {
+  reir: true,
+  comer: true,
+  llorar: false,
+};
+
+const reir = (guason: Guason): void => {
+  if (guason.reir) {
+    console.log('JAJAJAJA');
+  }
+};
+
+// Cree una interfaz para la siguiente funcion
+
+interface Ciudad {
+  (ciudadanos: string[]): number;
+}
+
+const ciudadGotica: Ciudad = (ciudadanos: string[]): number => {
+  return ciudadanos.length;
+};
+
+// Cree una interfaz que obligue crear una clase
+// con las siguientes propiedades y metodos
+
+/*
+  propiedades:
+    - nombre
+    - edad
+    - sexo
+    - estadoCivil
+    - imprimirBio(): void // en consola una breve descripcion.
+*/
+
+interface Individuo {
+  nombre: string;
+  edad: number;
+  sexo: string;
+  estadoCivil: string;
+  imprimirBio(): void;
+}
+class Persona implements Individuo {
+  constructor(
+    public nombre: string,
+    public edad: number,
+    public sexo: string,
+    public estadoCivil: string
+  ) {}
+  imprimirBio() {}
+}
