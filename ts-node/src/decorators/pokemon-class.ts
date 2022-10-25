5f45d28127b83be44e6dcc60248948e28cf3b374
Miguel Ángel Gallart <magallart58@gmail.com>
2022-10-25 12:24:38 +0200

    decoradores de métodos

--- ts-node/src/decorators/pokemon-class.ts
+++ ts-node/src/decorators/pokemon-class.ts
@@ -15,9 +15,31 @@ const bloquearPrototipo = function (constructor: Function) {
   Object.seal(constructor.prototype);
 };
 
+function CheckValidPokemonId() {
+  return function (
+    target: any,
+    propertyKey: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const originalMethod = descriptor.value;
+    descriptor.value = (id: number) => {
+      if (id < 1 || id > 800) {
+        return console.error('Id inválido. Debe estar entre 1 y 800');
+      } else {
+        return originalMethod(id);
+      }
+    };
+  };
+}
+
 @bloquearPrototipo
-@printToConsoleConditional(true)
+@printToConsoleConditional(false)
 export class Pokemon {
   public publicApi: string = 'https://pokeapi.co/';
   constructor(public name: string) {}
+
+  @CheckValidPokemonId()
+  savePokemonToDB(id: number) {
+    console.log(`Pokemon guardado en la base de datos ${id}`);
+  }
 }
